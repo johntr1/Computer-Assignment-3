ca4c2f868d5187ad7b3c9684a1c5aba014567d94
john-tran <johntran1@hotmail.se>
2023-02-28 22:27:19 +0100

    pokerv iew

--- pokerview.py
+++ pokerview.py
@@ -11,31 +11,81 @@ import sys
 # QErrorMessage
 
 
-
 class PlayerView(QGroupBox):
-    def __init__(self, num, player):
+    def __init__(self, player):
+        super().__init__()
+        self.player = player  # player is player name
+        layout = QVBoxLayout()
+        player_turn = QLabel()
+        player_turn.setText(self.player)
+        layout.addWidget(player_turn)
+        self.setLayout(layout)
+
+
+class PokerButtons(QGroupBox):
+    def __init__(self):
         super().__init__()
 
-
-app = QApplication([])
-app.setStyle('Fusion')
-
-
-def show_popup():
-    msg = QMessageBox()
-    msg.setWindowTitle("Set")
-    msg.exec()
+        button = QPushButton('Check')
+        layout = QHBoxLayout()
+        layout.addWidget(button)
+        layout.addWidget(QPushButton('Bet'))
+        layout.addWidget(QPushButton('Fold'))
+        self.setLayout(layout)
 
 
-button = QPushButton('Check')
-layout = QHBoxLayout()
-layout.addWidget(button)
-layout.addWidget(QPushButton('Bet'))
-layout.addWidget(QPushButton('Fold'))
+class PokerBoardView(QWidget):
+    def __init__(self, poker):
+        super().__init__()
+        layout = QGridLayout()
+        self.setLayout(layout)
 
-button.clicked.connect(show_popup())
 
-window = QWidget()
-window.setLayout(layout)
+class PokerView(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QGridLayout()
+
+        layout.addWidget(PlayerView(player), 2, 3, 1, 2)
+        layout.addWidget(PokerButtons(),0,1)
+
+        phbox = QHBoxLayout()
+        phbox.addWidget(PokerButtons())
+
+        self.setLayout(layout)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+        self.setWindowTitle("CA3 Group 19")
+        status = QStatusBar()
+        status.addWidget(PlayerView(player))
+        self.setStatusBar(status)
+
+        # Menu Bars
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu('&File')
+
+        # Tool bars
+        toolbar = QToolBar("Poker Actions")
+        toolbar.setIconSize(QSize(16,16))
+        self.addToolBar(toolbar)
+
+
+player = "John"
+
+"""
+app = QApplication(sys.argv)
+import sys
+
+window = MainWindow(app)
 window.show()
-app.exec()
+app.exec_()
+"""
+
+app = QApplication(sys.argv)
+window = PokerView()
+window.show()
+app.exec_()
